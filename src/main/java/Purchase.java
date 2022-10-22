d9f24faa3efa0a1c857ab54b25d5f36c42f4a96f
Hitray <75577433+Hitray@users.noreply.github.com>
2022-10-22 12:47:44 +0500

    Create Purchase.java

---
+++ src/main/java/Purchase.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Purchase {
+ 
+    private final Map<String, Integer> products;
+    private final Map<String, Integer> purchases = new HashMap<>();
+
+
+
+
+
+ 
+    public Purchase() {
+        // Dependency Inversion Principle - в принципе сюда подходит
+        // описываем интерфейс, скрывая реализацию (может там вообще SQL)
+        DataBase dataBase = Data.getInstance();
+        products = dataBase.getProducts();
+    }
+
+    public void addPurchase(String title, int count) {
+        // Тут как всегда - добавляем или апдейтим энтри продаж
+        purchases.put(title, purchases.getOrDefault(title, 0) + count);
+
+    }
+
+    public void printPurchases() {
+
+        long sum = 0;
+        System.out.println("КОРЗИНА:");
+        for (Map.Entry<String, Integer> entryMap : purchases.entrySet()) {
+            System.out.println("\t" + entryMap.getKey() + " " + entryMap.getValue()
+                    + " шт. в сумме " + (entryMap.getValue() * products.get(entryMap.getKey()) + " руб."));
+            sum += (long) entryMap.getValue() * products.get(entryMap.getKey());
+        }
+        System.out.println("ИТОГО: " + sum);
+    }
+
+
+  
+    public void printShowCase() {
+        System.out.println("В МАГАЗИНЕ В НАЛИЧИИ");
+        for (Map.Entry<String, Integer> productAndPrice : products.entrySet()) {
+            System.out.println(productAndPrice.getKey() + " за " + productAndPrice.getValue() + " руб./шт.");
+        }
+        System.out.println("Введите два слова: название товара и количество. Или end");
+    }
+}
