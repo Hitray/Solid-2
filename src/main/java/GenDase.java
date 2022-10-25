6f7decd068c67323de4c41a642fc40bb7db13871
Hitray <75577433+Hitray@users.noreply.github.com>
2022-10-25 20:32:54 +0500

    Update GenDase.java

--- src/main/java/GenDase.java
+++ src/main/java/GenDase.java
@@ -1,6 +1,22 @@
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-// Отдельный класс
-public interface GenDase {
-    Map<String, Integer> getProducts();
+public class GenDase {
+    public GenDase(List<Purchase> purchases) {
+
+    }
+
+    public static void showAllProductsPricesAndAmount(Map<String, Integer> prices, List<Purchase> purchases){
+        System.out.println("КОРЗИНА:");
+        for (int i = 0; i < purchases.size(); i++) {
+            Purchase purchase = purchases.get(i);
+            if (purchase == null) continue;
+            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + (purchase.count * prices.get(purchase.title)) + " руб.");
+        }
+    }
+
+    public long sum(HashMap<String, Integer> products) {
+        return 0;
+    }
 }
